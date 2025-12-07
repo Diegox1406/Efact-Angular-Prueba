330f542cf94912e06625bff79ce8ca3081fc95d9
Diegox1406 <pazsoldan1320@gmail.com>
2025-12-07 12:19:52 -0500

    Creando componentes y rutas

---
+++ src/app/documents/documents-view/documents-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-documents-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './documents-view.component.html',
+  styleUrl: './documents-view.component.css'
+})
+export class DocumentsViewComponent {
+
+}
---
+++ src/app/documents/documents.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { DocumentsViewComponent } from "./documents-view/documents-view.component";
+
+export const DOCUMENTS_ROUTES: Routes = [
+    {path: 'documents', component: DocumentsViewComponent}
+];
