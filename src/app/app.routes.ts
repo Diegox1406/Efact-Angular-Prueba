ac53777ff962edc403c99afff47787cd2fee305f
Diegox1406 <pazsoldan1320@gmail.com>
2025-12-08 03:00:36 -0500

    Redirigir al login automaticamente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: 'login',
         loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES)
     },
     {
         path: 'documents',
         loadChildren: () => import('./documents/documents.routes').then(m => m.DOCUMENTS_ROUTES)
+    },
+    { 
+        path: '**', redirectTo: 'login' 
     }
 ];
