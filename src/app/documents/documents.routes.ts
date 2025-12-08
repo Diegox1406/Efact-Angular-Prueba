1d0c656cb90308e01f4ad98de08343098a92692a
Diegox1406 <pazsoldan1320@gmail.com>
2025-12-08 02:56:09 -0500

    Protegiendo ruta de documents

--- src/app/documents/documents.routes.ts
+++ src/app/documents/documents.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { DocumentsViewComponent } from "./documents-view/documents-view.component";
+import { authGuard } from "../core/guards/auth.guard";
 
 export const DOCUMENTS_ROUTES: Routes = [
-    {path: 'documents', component: DocumentsViewComponent}
+    {path: 'documents', component: DocumentsViewComponent, canActivate: [authGuard]}
 ];
